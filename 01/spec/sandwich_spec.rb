82e2b7c93f63f07c8ba57ac73c8e598324bf34ec
Alexei <alexeim763@gmail.com>
2018-09-30 21:13:17 -0500

    finish first example

--- 01/spec/sandwich_spec.rb
+++ 01/spec/sandwich_spec.rb
@@ -1,7 +1,22 @@
+Sandwich = Struct.new(:taste, :toppings)
+
 RSpec.describe 'An ideal Sandwich' do
+  # before { @sandwich = Sandwich.new('delicius', []) }
+
+  # def sandwich
+  #   @sandwich ||= Sandwich.new('delicius', [])
+  # end
+
+  let(:sandwich) {Sandwich.new('delicius', [])}
   it 'is delicius' do
-    sandwich = Sandwich.new('delicius', [])
     taste = sandwich.taste
-    expect(taste).to eq('delicius')  
+    expect(taste).to eq('delicius')
+  end
+ 
+  it 'lets me add toppings' do
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
   end
 end
