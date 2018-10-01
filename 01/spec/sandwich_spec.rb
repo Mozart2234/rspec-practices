7b464f9b6299a9dc3c5c022613e33cdd10164a9e
Alexei <alexeim763@gmail.com>
2018-09-30 20:42:18 -0500

    ...

--- 01/spec/sandwich_spec.rb
+++ 01/spec/sandwich_spec.rb
@@ -1,4 +1,7 @@
 Rspec.describe 'An ideal Sandwich' do
   it 'is delicius' do
+    sandwich = Sandwich.new('delicius', [])
+    taste = sandwich.taste
+    expect(taste).to eq('delicius')  
   end
 end
