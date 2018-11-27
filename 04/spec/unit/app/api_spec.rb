49eed8c94c66466f113ec9f5c03f71366f1a28cb
Alexei <alexeim763@gmail.com>
2018-11-26 23:13:47 -0500

    added more conf and test_snippets

--- 04/spec/unit/app/api_spec.rb
+++ 04/spec/unit/app/api_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../../../app/api'
+require 'rack/test'
 
 module ExpenseTracker
   RSpec.describe API do
     describe 'POST /expenses' do
-      context "when the expense is successfully recorded" do
+      context 'when the expense is successfully recorded' do
         it 'returns the expense id'
         it 'responds with a 200(OK)'
       end
 
-      context "when the expense fails validation" do
+      context 'when the expense fails validation' do
         it 'returns an error message'
         it 'responds with a 422 (Unprocessable entity)'
       end
-      
     end
   end
 end
