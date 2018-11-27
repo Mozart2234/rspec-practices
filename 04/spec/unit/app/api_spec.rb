9b31b97dac4f14c421b9a952aa8fe80b65f4bae5
Alexei <alexeim763@gmail.com>
2018-11-26 23:44:31 -0500

    added unit test for respond 200

--- 04/spec/unit/app/api_spec.rb
+++ 04/spec/unit/app/api_spec.rb
@@ -7,7 +7,7 @@ module ExpenseTracker
   RSpec.describe API do
 
     include Rack::Test::Methods
-    
+
     def app
       API.new(ledger: ledger)
     end
@@ -28,7 +28,15 @@ module ExpenseTracker
           expect(parsed).to include('expense_id' => 417)
         end
 
-        it 'responds with a 200(OK)'
+        it 'responds with a 200(OK)' do
+          expense = { 'some' => 'data' }
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true,417,nil))
+
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
       end
 
       context 'when the expense fails validation' do
