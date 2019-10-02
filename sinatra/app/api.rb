0eeff075c86e70385bb545a975319fa3a07a954a
Alexei Mamani <alexeim763@gmail.com>
2019-10-01 22:09:27 -0500

    Config rspec

--- sinatra/app/api.rb
+++ sinatra/app/api.rb
@@ -2,6 +2,13 @@ require 'sinatra/base'
 require 'json'
 
 module ExpenseTracker
-  class API < SinatraBase
+  class API < Sinatra::Base
+    post '/expenses' do
+      JSON.generate('expense_id' => 42)
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
   end
 end
